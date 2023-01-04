28febe6263f9789a9866f904a3bc728f2faa8e1f
Rukundojoseph <104127964+Rukundojoseph@users.noreply.github.com>
2023-01-04 04:08:10 -0900

    fix: fixed the user unique

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,22 @@
 import mongoose from 'mongoose'
-import joi from 'joi'
 import  bcrypt from 'bcrypt';
+
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
 
 const userSchema = new mongoose.Schema({
 
-    email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    email:{
+      type: String,
+      trim: true,
+      lowercase: true,  
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+      unique: [true,'email is already registered'],
+  },
   password: {
     type: String,
     required: [true, 'Please enter a password'],
